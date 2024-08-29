2c74e1b23b2a81729ab05081518bf346d5bc8b46
LeeTaeHyun <apth1023@naver.com>
2024-08-29 09:16:36 +0900

    RoomBase Add

---
+++ Source/ShootingGame3D/Gimmick/RoomBase.h
@@ -0,0 +1,60 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "RoomBase.generated.h"
+
+UCLASS()
+class SHOOTINGGAME3D_API ARoomBase : public AActor
+{
+	GENERATED_BODY()
+	
+public:
+	ARoomBase();
+
+protected:
+	virtual void BeginPlay() override;
+
+public:
+	virtual void Tick(float DeltaTime) override;
+
+// Init
+private:
+	void CreateMeshComponent();
+	void InitRoomTransform();
+
+// Components
+private:
+	// Collision
+	UPROPERTY(VisibleAnywhere, Category = "Room")
+	TObjectPtr<class UBoxComponent> BoxComp;
+
+	// Mesh
+	UPROPERTY(VisibleAnywhere, Category = "Room")
+	TObjectPtr<class UStaticMeshComponent> FloorMesh;
+
+	UPROPERTY(VisibleAnywhere, Category = "Room")
+	TArray<TObjectPtr<class UStaticMeshComponent>> WallMeshes;
+
+	UPROPERTY(VisibleAnywhere, Category = "Room")
+	TObjectPtr<class UStaticMeshComponent> Left_Door;
+	UPROPERTY(VisibleAnywhere, Category = "Room")
+	TObjectPtr<class UStaticMeshComponent> Left_Wall;
+
+	UPROPERTY(VisibleAnywhere, Category = "Room")
+	TObjectPtr<class UStaticMeshComponent> Right_Door;
+	UPROPERTY(VisibleAnywhere, Category = "Room")
+	TObjectPtr<class UStaticMeshComponent> Right_Wall;
+
+	UPROPERTY(VisibleAnywhere, Category = "Room")
+	TObjectPtr<class UStaticMeshComponent> Up_Door;
+	UPROPERTY(VisibleAnywhere, Category = "Room")
+	TObjectPtr<class UStaticMeshComponent> Up_Wall;
+
+	UPROPERTY(VisibleAnywhere, Category = "Room")
+	TObjectPtr<class UStaticMeshComponent> Down_Door;
+	UPROPERTY(VisibleAnywhere, Category = "Room")
+	TObjectPtr<class UStaticMeshComponent> Down_Wall;
+};
